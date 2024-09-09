4bd8279fcc22c4e5a2f5936924adb60d464ada22
IGuidoo <iguidoo@outlook.com>
2024-09-09 13:20:39 +0200

    breadcrumbs gekloot

--- app/app/settings/_components/BreadCrumbs.tsx
+++ app/app/settings/_components/BreadCrumbs.tsx
@@ -4,17 +4,16 @@ import Link from "next/link";
 import BackAndForwardButtons from "./BackAndForwardButtons";
 
 import { ChevronRightIcon } from "@heroicons/react/20/solid";
+import { BreadCrumbItem } from "../layout";
 
 
 const BreadCrumbsSettings = ({
-  path,
-  secondPath,
-  thirdPath
+  breadCrumbs
 }: {
-  path?: string;
-  secondPath?: string | null;
-  thirdPath?: string | null;
+  breadCrumbs?: BreadCrumbItem[];
 }) => {
+
+  console.log('breadCrumbs', breadCrumbs)
 
   // console.log('path', path)
   // console.log('secondPath', secondPath)
@@ -23,10 +22,18 @@ const BreadCrumbsSettings = ({
   return (
     <div className="sticky top-0 z-10 px-6 py-4 w-full bg-white dark:bg-dark-bg-light rounded-t-xl border-b border-light-stroke dark:border-dark-stroke flex justify-between items-center">
       <p className="inline-flex items-center gap-4 text-sm leading-5 font-base text-light-text-light dark:text-dark-text-dark">
-        <Link href="/app/">Home</Link>
+        {/* <Link href="/app/">Home</Link>
         <span><ChevronRightIcon className="w-4 h-4" /></span>
-        <Link href="/app/settings">Settings</Link>
-        {path !== 'Settings' && path && (
+        <Link href="/app/settings">Settings</Link> */}
+
+        {breadCrumbs && breadCrumbs.map((crumb, index) => (
+          <React.Fragment key={index}>
+            <span><ChevronRightIcon className="w-4 h-4" /></span>
+            <Link href={crumb.href}>{crumb.name}</Link>
+          </React.Fragment>
+        ))}
+
+        {/* {path !== 'Settings' && path && (
           <>
             <span><ChevronRightIcon className="w-4 h-4" /></span>
             <span className="text-[#2C3462] dark:text-gray-500">{path}</span>
@@ -43,7 +50,7 @@ const BreadCrumbsSettings = ({
             <span><ChevronRightIcon className="w-4 h-4" /></span>
             <span className="text-[#2C3462] dark:text-gray-500">{thirdPath}</span>
           </>
-        )}
+        )} */}
       </p>
       <BackAndForwardButtons />
     </div>
--- app/app/settings/layout.tsx
+++ app/app/settings/layout.tsx
@@ -1,45 +1,59 @@
 'use client';
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { usePathname } from 'next/navigation';
 
 import BreadCrumbsSettings from './_components/BreadCrumbs'
 import PageTitle from './_components/PageTitle';
+import { useWebsiteDetailsStore } from '@/app/_stores/useWebsiteDetailsStore';
+import { WebsiteWithLocation } from '@/src/entities/models/website';
+
+const UUID_LENGTH = 25;
+
+export type BreadCrumbItem = {
+  name: string;
+  href: string;
+  current: boolean;
+};
+
+
+function capitalizedPath(path: string) {
+  return path.charAt(0).toUpperCase() + path.slice(1);
+}
 
 export default function Layout(
   { children }: { children: React.ReactNode }
 ) {
   const pathname = usePathname();
-  const lastPath = pathname.split("/").filter(Boolean).pop() || "";
+  const websitesStore = useWebsiteDetailsStore(state => state.websites);
 
+  const lastPath = pathname.split("/").filter(Boolean).pop() || "";
   const pathSegments = pathname.split("/").filter(Boolean);
   const secondToLastPath = pathSegments.length > 1 ? pathSegments[pathSegments.length - 2] : "";
+  const [pageTitle, setPageTitle] = useState<string>(capitalizedPath(lastPath))
+  const [breadCrumbs, setBreadCrumbs] = useState<BreadCrumbItem[]>([])
 
-  // console.log('pathname ------------------------------------', pathname)
-  // console.log('lastPath', lastPath)
-  // console.log('secondToLastPath', secondToLastPath)
+  useEffect(() => {
+    console.log('useEffect path', pathname)
+
+    const pageTTitle = getPageTitle(lastPath, secondToLastPath, websitesStore)
+    const breadCrumbs = generateBreadCrumbs2(pathSegments, pageTTitle)
+    console.log('pageTTitle', pageTTitle)
+    console.log('generateBreadCrumbs', breadCrumbs)
+    setPageTitle(pageTTitle)
+    setBreadCrumbs(breadCrumbs)
+
+    console.log('pathSegments', pathSegments)
 
 
+    // setBreadCrumbs(newBreadCrumb)
+  }, [pathname])
 
-  let capitalizedPath = null
-  let secondPath = null  
-  let thirdPath = null
-
-  if (secondToLastPath === "website") {
-    capitalizedPath = 'Website';
-    secondPath = lastPath.charAt(0).toUpperCase() + lastPath.slice(1).toLowerCase();
-  } else if (secondToLastPath === 'location'){
-    capitalizedPath = 'Website';
-    secondPath = secondToLastPath.charAt(0).toUpperCase() + secondToLastPath.slice(1).toLowerCase();
-    thirdPath = lastPath.charAt(0).toUpperCase() + lastPath.slice(1).toLowerCase();
-  } else {
-    capitalizedPath = lastPath.charAt(0).toUpperCase() + lastPath.slice(1).toLowerCase();
-  }
 
   return (
     <>
-      <BreadCrumbsSettings path={capitalizedPath} secondPath={secondPath} thirdPath={thirdPath} />
-      <PageTitle path={capitalizedPath} />
+      <BreadCrumbsSettings breadCrumbs={breadCrumbs} />
+      <PageTitle path={pageTitle} />
       <div className='max-w-[918px] mx-auto py-5'>
         {children}
       </div>
@@ -47,3 +61,101 @@ export default function Layout(
   )
 }
 
+function getPageTitle(path: string, secondToLastPath: string, websitesStore: WebsiteWithLocation[] | undefined): string {
+  if (path.length === UUID_LENGTH) {
+    console.log('secondToLastPath', secondToLastPath);
+
+    // Set location as page title
+    if (secondToLastPath === 'location') {
+      if (!websitesStore) return capitalizedPath(path);
+      const websiteWithLocation = websitesStore.find(website =>
+        website.location?.some(location => location.id === path)
+      );
+
+      const locationString = websiteWithLocation?.location?.find(location => location.id === path)?.location;
+      if (locationString) {
+        return capitalizedPath(locationString);
+      }
+
+      const country = websiteWithLocation?.location?.find(location => location.id === path)?.country;
+      if (country) {
+        return capitalizedPath(country);
+      }
+
+      return capitalizedPath(path); // Default return value if no location or country is found
+    }
+
+    if (secondToLastPath === 'website') {
+      if (!websitesStore) return capitalizedPath(path);
+      const website = websitesStore.find(website => website.id === path);
+      console.log('website', website);
+      if (website) {
+        return capitalizedPath(website.websiteName);
+      }
+      return capitalizedPath(path); // Default return value if no website is found
+    }
+    return capitalizedPath(path);
+  }
+  return capitalizedPath(path);
+}
+
+function generateBreadCrumbs(pathSegments: string[], pageTitle: string) {
+  const newBreadCrumb = pathSegments.map((path, index) => {
+    if (path === 'app') {
+      return { name: 'Home', href: '/app', current: false };
+    }
+    // Accumulate the path segments to build the correct href
+    const href = `/${pathSegments.slice(0, index + 1).join('/')}`;
+    if (path.length === UUID_LENGTH) {
+      return { name: pageTitle, href, current: false };
+    }
+    // If the last path segment is a UUID, set the page title to the capitalized path
+    if (index === pathSegments.length - 1) {
+      if (path.length === UUID_LENGTH) {
+        return { name: pageTitle, href, current: true };
+      }
+    }
+
+    return { name: path, href, current: false };
+  });
+
+  return newBreadCrumb
+}
+
+function generateBreadCrumbs2(pathSegments: string[], pageTitle: string) {
+  const newBreadCrumb = pathSegments.map((path, index) => {
+    if (path === 'app') {
+      return { name: 'Home', href: '/app', current: false };
+    }
+
+    if (path === 'settings') {
+      return { name: 'Settings', href: '/app/settings', current: false };
+    }
+
+    if (path === 'integrations') {
+      return { name: 'Integrations', href: '/app/settings/integrations', current: false };
+    }
+
+    if (index === pathSegments.length - 1){
+      if (path === 'website') {
+        return { name: pageTitle, href: '/app/settings/website', current: false };
+      }
+    }
+
+    // // Accumulate the path segments to build the correct href
+    // const href = `/${pathSegments.slice(0, index + 1).join('/')}`;
+    // if (path.length === UUID_LENGTH) {
+    //   return { name: pageTitle, href, current: false };
+    // }
+    // // If the last path segment is a UUID, set the page title to the capitalized path
+    // if (index === pathSegments.length - 1) {
+    //   if (path.length === UUID_LENGTH) {
+    //     return { name: pageTitle, href, current: true };
+    //   }
+    // }
+
+    return { name: path, href: 'h', current: false };
+  });
+
+  return newBreadCrumb
+}
