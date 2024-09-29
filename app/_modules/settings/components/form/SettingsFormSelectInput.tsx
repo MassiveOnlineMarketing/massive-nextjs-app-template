c399159d60a373ed4840bdded4df806710ad395b
IGuidoo <iguidoo@outlook.com>
2024-09-29 13:43:00 +0200

    styles: form input select

--- app/_modules/settings/components/form/SettingsFormSelectInput.tsx
+++ app/_modules/settings/components/form/SettingsFormSelectInput.tsx
@@ -91,7 +91,9 @@ const FormInputSelectContent = React.forwardRef<
     <SelectPrimitive.Content
       ref={ref}
       className={cn(
-        "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+        "z-50 min-w-[8rem] overflow-hidden rounded-[8px] border theme-b-p theme-bg-w p-1 ",
+
+        'data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
         position === "popper" &&
         "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
         className
@@ -102,7 +104,6 @@ const FormInputSelectContent = React.forwardRef<
       <SelectScrollUpButton />
       <SelectPrimitive.Viewport
         className={cn(
-          "p-1",
           position === "popper" &&
           "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
         )}
@@ -121,7 +122,7 @@ const FormInputSelectLabel = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SelectPrimitive.Label
     ref={ref}
-    className={cn("px-2 py-1.5 text-sm font-semibold", className)}
+    className={cn("p-2 text-xs font-medium theme-t-n", className)}
     {...props}
   />
 ));
@@ -134,7 +135,9 @@ const FormInputSelectItem = React.forwardRef<
   <SelectPrimitive.Item
     ref={ref}
     className={cn(
-      "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-2 pr-8 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50 focus:bg-neutral-100 dark:focus:bg-neutral-800",
+      "relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm theme-t-p outline-none",
+      // Background
+      'focus:bg-base-50 data-[state=open]:bg-base-50 dark:focus:bg-theme-night-background-secondary dark:data-[state=open]:bg-theme-night-background-secondary data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
       className,
     )}
     {...props}
@@ -155,7 +158,10 @@ const FromInputSelectSeparator = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SelectPrimitive.Separator
     ref={ref}
-    className={cn("-mx-1 my-1 h-px bg-muted", className)}
+    className={cn(
+      "-mx-1 my-1 h-px bg-theme-light-stroke dark:bg-theme-night-stroke",
+      className
+    )}
     {...props}
   />
 ));
