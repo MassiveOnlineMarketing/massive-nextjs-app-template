e2319eaafaae3c976ac1ee56e2caf940016dff84
IGuidoo <iguidoo@outlook.com>
2024-09-30 22:27:29 +0200

    fix: build error mock api

--- src/infrastructure/api/search-console.api.mock.ts
+++ src/infrastructure/api/search-console.api.mock.ts
@@ -2,11 +2,16 @@ import { injectable } from "inversify";
 
 import { ISearchConsoleApi } from "@/src/application/api/search-console.api.interface";
 
-import { FetchConnectedSitesResponseGoogleSearchApi } from "@/src/application/api/search-console.api.types";
+import {
+  FetchConnectedSitesResponseGoogleSearchApi,
+  SearchConsoleApiFetchKeywordDataSuccessResponse,
+} from "@/src/application/api/search-console.api.types";
 
 @injectable()
 export class MockSearchConsoleApi implements ISearchConsoleApi {
-  async fetchConnectedSites(refreshToken: string): Promise<FetchConnectedSitesResponseGoogleSearchApi[]> {
+  async fetchConnectedSites(
+    refreshToken: string
+  ): Promise<FetchConnectedSitesResponseGoogleSearchApi[]> {
     return [
       {
         permissionLevel: "siteFullUser",
@@ -33,6 +38,56 @@ export class MockSearchConsoleApi implements ISearchConsoleApi {
     amountOfKeywords: string,
     countryCode: string
   ) {
-    return ['keyword1', 'keyword2', 'keyword3', 'keyword4', 'keyword5'];
+    return ["keyword1", "keyword2", "keyword3", "keyword4", "keyword5"];
+  }
+
+  async fetchKeywordData(
+    refreshToken: string,
+    siteProperty: string,
+    keyword: string,
+    url: string,
+    startDate: string,
+    endData: string
+  ): Promise<SearchConsoleApiFetchKeywordDataSuccessResponse> {
+    return {
+      data: [
+        {
+          date: "2021-01-01",
+          clicks: 1,
+          impressions: 2,
+          ctr: 3,
+          position: 4,
+        },
+        {
+          date: "2021-01-02",
+          clicks: 2,
+          impressions: 3,
+          ctr: 4,
+          position: 5,
+        },
+        {
+          date: "2021-01-03",
+          clicks: 3,
+          impressions: 4,
+          ctr: 5,
+          position: 6,
+        },
+        {
+          date: "2021-01-04",
+          clicks: 4,
+          impressions: 5,
+          ctr: 6,
+          position: 7,
+        },
+        {
+          date: "2021-01-05",
+          clicks: 5,
+          impressions: 6,
+          ctr: 7,
+          position: 8,
+        },
+      ],
+      status: "success",
+    };
   }
 }
