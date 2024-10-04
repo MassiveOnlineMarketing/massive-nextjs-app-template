e56a4f7681bcc5ab90d5ab3960bb4c9942362c46
IGuidoo <iguidoo@outlook.com>
2024-10-04 14:35:26 +0200

    chore: redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
+import { redirect, RedirectType } from "next/navigation";
 
 export default function Home() {
+  redirect("/app", RedirectType.replace);
   return (
     <div>
       home
